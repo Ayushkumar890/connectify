bbad3c92bdbdd27047a77e61eee420f339b7e7d8
Ayushkumar890 <ayushkumar.hp890@gmail.com>
2024-08-30 09:20:45 +0530

    app updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Otp from './components/Otp';
-import Login from './components/Login';
-import Signup from './components/Signup';
+
+import { router } from './routers/Routes';
+import { RouterProvider } from 'react-router-dom';
 
 function App() {
     return (
-        <Router>
-            <Routes>
-                <Route path="/signup" element={<Signup />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/otp" element={<Otp />} />
-            </Routes>
-        </Router>
+        <>
+            <RouterProvider router={router} />
+        </>
+
     );
 }
 
