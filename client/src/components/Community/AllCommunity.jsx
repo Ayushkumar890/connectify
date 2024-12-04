f843c8c2235423d4b919814ed13421c12c9e5c90
Ayushkumar890 <ayushkumar.hp890@gmail.com>
2024-12-04 10:31:17 +0530

    update allcommunity

---
+++ client/src/components/Community/AllCommunity.jsx
@@ -0,0 +1,41 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import CommunityBox from './CommunityBox';
+
+const AllCommunity = () => {
+    const [communities, setCommunities] = useState([]);
+
+    useEffect(() => {
+        const fetchUserData = async () => {
+          try {
+            const response = await axios.get("http://localhost:3000/community", {
+              withCredentials: true,
+            });
+    
+            console.log("API response:", response.data); 
+            setCommunities(response.data.communities);
+            console.log("Communities data fetched:", response.data.communities);
+          } catch (error) {
+            console.error("Error fetching user data:", error);
+          }
+        };
+    
+        fetchUserData();
+      }, []);
+
+
+  return (
+    <div>
+        <div className='grid grid-cols-1 md:grid-cols-2 gap-4"'> 
+            {communities.map((community) => (
+             
+                <CommunityBox key={community._id} community={community}  />
+            ))}
+        </div>
+        
+
+    </div>
+  )
+}
+
+export default AllCommunity
