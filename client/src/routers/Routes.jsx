ab4f4ca62547b53ea606c5e785d8985d8769e9c1
Ayushkumar890 <ayushkumar.hp890@gmail.com>
2024-12-04 10:33:40 +0530

    update routes

--- client/src/routers/Routes.jsx
+++ client/src/routers/Routes.jsx
@@ -1,8 +1,7 @@
 import Chat from "../components/ChatsFeature/Chat";
 import ChatBox from "../components/ChatsFeature/ChatBox";
-// import AllCommunity from "../components/community/AllCommunity";
-// import CreateCommunity from "../components/community/CreateCommunity";
-// import MyCommunity from "../components/Community/MyCommunity";
+import AllCommunity from "../components/Community/AllCommunity";
+import CreateCommunity from "../components/Community/CreateCommunity";
 import Home from "../components/Home";
 import Layout from "../components/Layout";
 import MyPost from "../components/Mypost/MyPost";
@@ -14,6 +13,10 @@ import Signup from "../components/usercreate/Signup";
 import UserProfile from "../components/UserProfile";
 import ProtectedRoute from './ProtectedRoute';
 import PublicRoute from './PublicRoute';
+import CommunityGroup from "../components/Community/CommunityGroup";
+import AllMembers from "../components/Community/AllMembers";
+import AdminDashboard from "../components/Dashboard/AdminDashboard";
+import MyCommunity from "../components/Dashboard/MyCommunity";
 
 const { Route, createRoutesFromElements, createBrowserRouter } = require("react-router-dom");
 
@@ -27,9 +30,12 @@ export const router = createBrowserRouter(
             <Route path="home" element={<ProtectedRoute> <TotalPost /></ProtectedRoute>} />
             <Route path="mypost" element={<ProtectedRoute> <MyPost /></ProtectedRoute>} />
             <Route path="chat" element={<ProtectedRoute> <Chat /></ProtectedRoute>} />
-            {/* <Route path="community/myCommunities" element={<ProtectedRoute> <MyCommunity /></ProtectedRoute>} /> */}
-            {/* <Route path="community" element={<ProtectedRoute> <AllCommunity/> </ProtectedRoute>} /> */}
-            {/* <Route path="community/create" element={<ProtectedRoute> <CreateCommunity /></ProtectedRoute>} /> */}
+            <Route path="community/myCommunities" element={<ProtectedRoute> <MyCommunity /></ProtectedRoute>} />
+            <Route path="community" element={<ProtectedRoute> <AllCommunity/> </ProtectedRoute>} />
+            <Route path="community/:communityId" element={<ProtectedRoute> <CommunityGroup/> </ProtectedRoute>} />
+            <Route path="community/:communityId/members" element={<ProtectedRoute> <AllMembers/> </ProtectedRoute>} />
+            <Route path="dashboard" element={<ProtectedRoute> <AdminDashboard/> </ProtectedRoute>} />
+            <Route path="community/create" element={<ProtectedRoute> <CreateCommunity /></ProtectedRoute>} />
             <Route path="chatbox" element={<ProtectedRoute> <ChatBox /></ProtectedRoute>} />
             <Route path="profile" element={<ProtectedRoute> <UserProfile /></ProtectedRoute>} />
             <Route path="search" element={<ProtectedRoute> <Search /></ProtectedRoute>} />
