c092397bc3f971d7def5bffa47b78431dec146ec
Ayushkumar890 <ayushkumar.hp890@gmail.com>
2024-12-04 10:30:14 +0530

    update landing page

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -26,15 +26,14 @@ function Home() {
           <div className="flex w-full md:w-1/2   ">
             <div className="w-full text-center md:text-left px-10 mt-10 space-y-2  mx-auto">
               <h1 className="text-green w-full  font-bold md:pt-16 text-4xl md:text-7xl">
-                CONNECTING Experts To People
+                CONNECTING Health Experts To People
               </h1>
               <p className="text-lg  text-gray-300">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                Facilis voluptates ducimus unde? Iste, quia maxime.
+              Empowering communities by connecting experts with people, fostering knowledge sharing, professional guidance, and meaningful interactions for personal growth.
               </p>
               <div className=" flex justify-center md:justify-start py-10 space-x-5">
                 <button onClick={handlelogin} className="relative overflow-hidden h-12 px-8 rounded-md border-2 border-green bg-green text-gray-200  cursor-pointer group">
-                  <span className="relative z-10">Login</span>
+                  <span className="relative z-10">Home</span>
                   <span className="absolute top-0 left-0 w-full h-full bg-gradient-to-r from-black to-black scale-x-0 group-hover:scale-x-100 transition-transform duration-[300ms] ease-in-out origin-right rounded-md"></span>
                 </button>
                 <button onClick={handlesignup} className="relative overflow-hidden h-12 px-8 rounded-md border-2  border-green text-gray-200   cursor-pointer group">
